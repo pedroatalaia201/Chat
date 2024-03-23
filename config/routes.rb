d3ce1e5d1d1173e9b8d560a4e8e178e86f3ea69a
Pedro <pedrocondatalaia@hotmail.com>
2024-03-23 00:01:16 -0300

    feat: add chatroom_users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
     devise_for :users
 
-    resources :chatrooms
+    resources :chatrooms do
+        # This is a nested route.
+        # This'll be the easiest way to get the id of th user and the chatroom.
+        resource :chatroom_users
+    end
 
     root to: "chatrooms#index"
 end
